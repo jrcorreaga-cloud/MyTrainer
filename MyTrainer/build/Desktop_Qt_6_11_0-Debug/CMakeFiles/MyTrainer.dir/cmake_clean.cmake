ef46dc5ce6562f132a38e8c89af8f00b47563f3c
jordan13 <jordan15cg@gmail.com>
2026-07-06 22:42:13 -0500

    Initial commit: Set up 3-tier architecture for MyTrainer project

---
+++ MyTrainer/build/Desktop_Qt_6_11_0-Debug/CMakeFiles/MyTrainer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTrainer.dir/main.cpp.o"
+  "CMakeFiles/MyTrainer.dir/main.cpp.o.d"
+  "MyTrainer"
+  "MyTrainer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyTrainer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
